65db51518157ff720274ed6537290e0c6a5a6804
Andy Sheley <andrew.sheley@gmail.com>
2018-10-15 22:10:34 -0500

    initial commit

---
+++ UnobtrusiveTest/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnobtrusiveTest.Models.Employee;
+
+namespace UnobtrusiveTest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public PartialViewResult _PartialName(Employee employee)
+        {
+            //...
+            //db.BookMasters.Add(one);
+            //db.SaveChanges();
+
+
+
+            return PartialView("_PartialAddress");
+        }
+
+        [HttpPost]
+        public PartialViewResult _PartialAddress(Employee employee)
+        {
+            //...
+            //db.Publisher.Add(two);
+            //db.SaveChanges();
+            return PartialView("_PartialAdditional");
+        }
+
+        [HttpPost]
+        public ActionResult _PartialAdditional(Employee employee)
+        {
+            //...
+            //db.Students.Add(Three);
+            //db.SaveChanges();
+            return RedirectToAction("Index", "Home");   //done
+        }
+    }
+}
---
+++ UnobtrusiveTest/Models/Employee/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnobtrusiveTest.Models.Employee
+{
+    public class Employee
+    {
+        
+        public int EmployeeID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address1 { get; set; }
+        
+        public string Address2 { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Zip { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
